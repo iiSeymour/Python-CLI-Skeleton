d9c1353d674346d415eef5348eff40c23ffba9eb
Chris Seymour <chris.j.seymour@hotmail.com>
2013-10-09 21:18:31 +0100

    Added -o and -i options

--- cli_skeleton.py
+++ cli_skeleton.py
@@ -10,8 +10,11 @@ def main(args, help):
         help()
         return
 
+    if args.output and args.output != '-':
+        sys.stdout = open(args.output, 'w')
+
     try:
-        for i, line in enumerate(fileinput.input(args.files)):
+        for i, line in enumerate(fileinput.input(args.files, inplace=args.inplace)):
             print i + 1, line.strip()
     except IOError:
         sys.stderr.write("%s: No such file %s\n" % (os.path.basename(__file__), fileinput.filename()))
@@ -20,4 +23,7 @@ if __name__ == "__main__":
     import os, sys, select, argparse, fileinput
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs='*', help='input files')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-i', '--inplace', action='store_true', help='modify files inplace')
+    group.add_argument('-o', '--output', help='output file. The default is stdout')
     main(parser.parse_args(), parser.print_help)
